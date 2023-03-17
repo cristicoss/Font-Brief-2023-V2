3bb2a44dd4cef2922ce26516dfb2adec97ae2ba5
cristicoss <64747154+cristicoss@users.noreply.github.com>
2023-03-17 20:30:44 +0100

    test FB 18

--- src/test.js
+++ src/test.js
@@ -1,25 +1,30 @@
 "use strict";
+
+///a mers in console
 const items = document.querySelectorAll(".item");
-const buttonFilter = document.querySelector(".button");
-console.log(buttonFilter);
+const button1 = document.querySelector(".button");
+const checkBox = document.getElementById("checkbox");
 
-// items.forEach(function (item) {
-//   const tBlock = item.querySelector(".t-block");
+button1.addEventListener("click", function () {
+  for (const item of items) {
+    const tBlock = item.querySelector(".t-block");
+    if (tBlock.innerText === "Giselle") {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  }
+});
 
-//   if (tBlock.innerText === "Eklips") {
-//     item.style.display = "block";
-//   } else {
-//     item.style.display = "none";
-//   }
-// });
-console.log(buttonFilter);
-// buttonFilter.addEventListener("click", function () {
-//   for (const item of items) {
-//     const tBlock = item.querySelector(".t-block");
-//     if (tBlock.innerText === "Eklips") {
-//       item.style.display = "block";
-//     } else {
-//       item.style.display = "none";
-//     }
-//   }
-// });
+checkBox.addEventListener("click", function () {
+  for (const item of items) {
+    const tBlock = item.querySelector(".t-block");
+    const label1 = checkBox.labels[0].textContent;
+    label1.textContent = "Giselle";
+    if (tBlock.innerText === label1) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  }
+});
