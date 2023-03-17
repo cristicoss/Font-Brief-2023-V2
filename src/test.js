68970bc9ee328e95174b13a3401bd4ce5c3ad351
cristicoss <64747154+cristicoss@users.noreply.github.com>
2023-03-17 19:47:36 +0100

    test FB 02

--- src/test.js
+++ src/test.js
@@ -1,12 +1,25 @@
 "use strict";
 const items = document.querySelectorAll(".item");
+const checkBox = document.querySelector(".checkbox");
 
-items.forEach(function (item) {
-  const tBlock = item.querySelector(".t-block");
+// items.forEach(function (item) {
+//   const tBlock = item.querySelector(".t-block");
 
-  if (tBlock.innerText === "Eklips") {
-    item.style.display = "block";
-  } else {
-    item.style.display = "none";
-  }
+//   if (tBlock.innerText === "Eklips") {
+//     item.style.display = "block";
+//   } else {
+//     item.style.display = "none";
+//   }
+// });
+
+checkBox.addEventListener("click", function () {
+  items.forEach(function (item) {
+    const tBlock = item.querySelector(".t-block");
+    const labelCheckbox = checkBox.querySelector(".label").innerText;
+    if (tBlock.innerText === labelCheckbox) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
 });
