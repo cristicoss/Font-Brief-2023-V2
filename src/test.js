a0f60cd284cbe0977bae43585abbb468bc254488
cristicoss <64747154+cristicoss@users.noreply.github.com>
2023-03-17 19:59:10 +0100

    test fb 08

--- src/test.js
+++ src/test.js
@@ -2,24 +2,24 @@
 const items = document.querySelectorAll(".item");
 const checkBox = document.getElementById("checkbox");
 
-items.forEach(function (item) {
-  const tBlock = item.querySelector(".t-block");
+// items.forEach(function (item) {
+//   const tBlock = item.querySelector(".t-block");
 
-  if (tBlock.innerText === "Eklips") {
-    item.style.display = "block";
-  } else {
-    item.style.display = "none";
-  }
-});
-
-// checkBox.addEventListener("click", function () {
-//   items.forEach(function (item) {
-//     const tBlock = item.querySelector(".t-block");
-//     const labelCheckbox = checkBox.querySelector(".label").innerText;
-//     if (tBlock.innerText === "Giselle") {
-//       item.style.display = "block";
-//     } else {
-//       item.style.display = "none";
-//     }
-//   });
+//   if (tBlock.innerText === "Eklips") {
+//     item.style.display = "block";
+//   } else {
+//     item.style.display = "none";
+//   }
 // });
+
+checkBox.addEventListener("click", function () {
+  items.forEach(function (item) {
+    const tBlock = item.querySelector(".t-block");
+    const labelCheckbox = checkBox.querySelector(".label").innerText;
+    if (tBlock.innerText === "Giselle") {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+});
