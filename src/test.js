cfe5ebeecabc24aafd5cd0d4d07f966e4d6c759d
cristicoss <64747154+cristicoss@users.noreply.github.com>
2023-03-17 20:14:22 +0100

    test fb 16

--- src/test.js
+++ src/test.js
@@ -13,21 +13,13 @@ console.log(buttonFilter);
 //   }
 // });
 console.log(buttonFilter);
-buttonFilter.addEventListener("click", function () {
-  for (const item of items) {
-    const tBlock = item.querySelector(".t-block");
-    if (tBlock.innerText === "Eklips") {
-      item.style.display = "block";
-    } else {
-      item.style.display = "none";
-    }
-    // items.forEach(function (item) {
-    //   const tBlock = item.querySelector(".t-block");
-    //   //const labelCheckbox = checkBox.querySelector(".label").innerText;
-    //   if (tBlock.innerText === "Eklips") {
-    //     item.style.display = "block";
-    //   } else {
-    //     item.style.display = "none";
-    //   }
-  }
-});
+// buttonFilter.addEventListener("click", function () {
+//   for (const item of items) {
+//     const tBlock = item.querySelector(".t-block");
+//     if (tBlock.innerText === "Eklips") {
+//       item.style.display = "block";
+//     } else {
+//       item.style.display = "none";
+//     }
+//   }
+// });
